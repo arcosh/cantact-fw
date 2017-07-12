b53773644d645d7a8c5ee87d862aa50123d6b8ec
Matthias <mail@matthiasbock.net>
2017-07-12 15:32:34 +0200

    Added error.h for global declaration of error codes

---
+++ Inc/error.h
@@ -0,0 +1,12 @@
+/**
+ * Declaration of error codes
+ */
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#define ERROR_SLCAN_COMMAND_NOT_RECOGNIZED  10
+#define ERROR_SLCAN_COMMAND_NOT_SUPPORTED   11
+#define ERROR_SLCAN_INVALID_ARGUMENT        12
+
+#endif // ERROR_H
