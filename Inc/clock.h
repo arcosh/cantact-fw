9448d4ae002875848ed09e7a7f058d68f091f39d
Matthias Bock <mail@matthiasbock.net>
2018-01-16 11:19:01 +0100

    Moved clock-related code to clock.c/.h and removed some code garbage from main.c

---
+++ Inc/clock.h
@@ -0,0 +1,8 @@
+
+#ifndef CLOCK_H
+#define CLOCK_H
+
+void SystemClock_Config();
+void MX_GPIO_Init();
+
+#endif
---
+++ Src/clock.c
@@ -0,0 +1,75 @@
+
+#include "clock.h"
+#include "stm32f0xx_hal.h"
+#include "config.h"
+
+
+void SystemClock_Config()
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+#ifdef INTERNAL_OSCILLATOR
+    // set up the oscillators
+    // use internal HSI48 (48 MHz), no PLL
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+    RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+
+    // set sysclk, hclk, and pclk1 source to HSI48 (48 MHz)
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK |
+                   RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+    // set USB clock source to HSI48 (48 MHz)
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+    PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+
+
+#elif defined EXTERNAL_OSCILLATOR
+    // set up the oscillators
+    // use external oscillator (16 MHz), enable 3x PLL (48 MHz)
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL3;
+    RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+
+    // set sysclk, hclk, and pclk1 source to PLL (48 MHz)
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK |
+                   RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+    // set USB clock source to PLL (48 MHz)
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+    PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLLCLK;
+
+#else
+    #error "Please define whether to use an internal or external oscillator"
+#endif
+
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
+    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
+    __SYSCFG_CLK_ENABLE();
+
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+}
+
+
+void MX_GPIO_Init()
+{
+    /* GPIO Ports Clock Enable */
+    __GPIOF_CLK_ENABLE();
+    __GPIOA_CLK_ENABLE();
+    __GPIOB_CLK_ENABLE();
+}
