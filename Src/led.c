4603a68fc8fcd99a17a192a9ff516e59a385e59c
Matthias Bock <mail@matthiasbock.net>
2018-01-26 17:09:30 +0100

    led.c: Added more #ifdefs to exclude code for disabled LEDs

--- Src/led.c
+++ Src/led.c
@@ -8,8 +8,12 @@
 /**
  * Tick count, when activity LED was last switched on
  */
+#ifdef LED_ACTIVITY_ENABLED
 static uint32_t activity_led_last_on = 0xFF;
+#endif
+#ifdef LED_ERROR_ENABLED
 static uint32_t error_led_last_on = 0xFF;
+#endif
 
 
 void led_init()
@@ -48,60 +52,78 @@ void led_init()
 
 void led_on(led_index_t led)
 {
+    #ifdef LED_POWER_ENABLED
     if (led == LED_POWER)
     {
         HAL_GPIO_WritePin(LED_POWER_PORT, LED_POWER_PIN, GPIO_PIN_SET);
         return;
     }
+    #endif
 
-	// Make sure the LED has been off for at least LED_DURATION before turning on again.
-	// This prevents a solid status LED on a busy canbus.
-	if ((led == LED_ACTIVITY)
-	 && (HAL_GetTick() - activity_led_last_on > 2*LED_ON_DURATION))
-	{
-		HAL_GPIO_WritePin(LED_ACTIVITY_PORT, LED_ACTIVITY_PIN, GPIO_PIN_RESET);
-		activity_led_last_on = HAL_GetTick();
-	}
+    #ifdef LED_ACTIVITY_ENABLED
+    // Make sure the LED has been off for at least LED_DURATION before turning on again.
+    // This prevents a solid status LED on a busy canbus.
+    if ((led == LED_ACTIVITY)
+     && (HAL_GetTick() - activity_led_last_on > 2*LED_ON_DURATION))
+    {
+        HAL_GPIO_WritePin(LED_ACTIVITY_PORT, LED_ACTIVITY_PIN, GPIO_PIN_RESET);
+        activity_led_last_on = HAL_GetTick();
+    }
+    #endif
 
-	if ((led == LED_ERROR)
-	 && (HAL_GetTick() - error_led_last_on > 2*LED_ON_DURATION))
-	{
+    #ifdef LED_ERROR_ENABLED
+    if ((led == LED_ERROR)
+     && (HAL_GetTick() - error_led_last_on > 2*LED_ON_DURATION))
+    {
         HAL_GPIO_WritePin(LED_ERROR_PORT, LED_ERROR_PIN, GPIO_PIN_SET);
         error_led_last_on = HAL_GetTick();
-	}
+    }
+    #endif
 }
 
 
 void led_off(led_index_t led)
 {
+    #ifdef LED_POWER_ENABLED
     if (led == LED_POWER)
     {
         HAL_GPIO_WritePin(LED_POWER_PORT, LED_POWER_PIN, GPIO_PIN_RESET);
         return;
     }
+    #endif
+
+    #ifdef LED_ACTIVITY_ENABLED
     if (led == LED_ACTIVITY)
     {
         HAL_GPIO_WritePin(LED_ACTIVITY_PORT, LED_ACTIVITY_PIN, GPIO_PIN_SET);
         return;
     }
+    #endif
+
+    #ifdef LED_ERROR_ENABLED
     if (led == LED_ERROR)
     {
         HAL_GPIO_WritePin(LED_ERROR_PORT, LED_ERROR_PIN, GPIO_PIN_RESET);
     }
+    #endif
 }
 
 
 void led_process()
 {
-	if ((HAL_GPIO_ReadPin(LED_ACTIVITY_PORT, LED_ACTIVITY_PIN) == GPIO_PIN_RESET)
+    #ifdef LED_ACTIVITY_ENABLED
+    if ((HAL_GPIO_ReadPin(LED_ACTIVITY_PORT, LED_ACTIVITY_PIN) == GPIO_PIN_RESET)
      && (HAL_GetTick() - activity_led_last_on > LED_ON_DURATION))
-	{
-		led_off(LED_ACTIVITY);
-	}
+    {
+        led_off(LED_ACTIVITY);
+    }
+    #endif
 
+    #ifdef LED_ERROR_ENABLED
     if ((HAL_GPIO_ReadPin(LED_ERROR_PORT, LED_ERROR_PIN) == GPIO_PIN_SET)
      && (HAL_GetTick() - error_led_last_on > LED_ON_DURATION))
     {
         led_off(LED_ERROR);
     }
+    #endif
 }
