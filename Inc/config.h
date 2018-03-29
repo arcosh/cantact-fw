5c2fbfe4f6c91a49b6010472d3eabb4ef32c7d00
Matthias Bock <mail@matthiasbock.net>
2018-03-29 18:41:20 +0200

    config.h: Oscillator is configured in platform.h

--- Inc/config.h
+++ Inc/config.h
@@ -7,9 +7,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-//#define INTERNAL_OSCILLATOR
-#define EXTERNAL_OSCILLATOR
-
 #define IRQ_PRIORITY_SYSTICK    3
 #define IRQ_PRIORITY_UART       2
 #define IRQ_PRIORITY_USB        2
